a11b35322d299aaec255797e0e85a5d3e1066f90
MerlinDassanayake <dinushadassa@gmail.com>
2025-08-06 19:37:43 -0400

    initial commit

---
+++ preprocessing/preprocessing_and_clustering.py
@@ -0,0 +1,61 @@
+# Core scverse libraries
+import scanpy as sc
+import anndata as ad
+
+# Data retrieval
+import pooch
+
+sc.settings.set_figure_params(dpi=50, facecolor='white')
+
+EXAMPLE_DATA = pooch.create(
+    path=pooch.os_cache('scverse_tutorials'),
+    base_url='doi:10.6084/m9.figshare.22716739.v1/',
+)
+EXAMPLE_DATA.load_registry_from_doi()
+
+samples = {
+    "s1d1": "s1d1_filtered_feature_bc_matrix.h5",
+    "s1d3": "s1d3_filtered_feature_bc_matrix.h5",
+}
+adatas = {}
+
+for sample_id, filename in samples.items():
+    path = EXAMPLE_DATA.fetch(filename)
+    sample_adata = sc.read_10x_h5(path)
+    sample_adata.var_names_make_unique()
+    adatas[sample_id] = sample_adata
+
+adata = ad.concat(adatas, label='sample')
+adata.obs_names_make_unique
+print(adata.obs["sample"].value_counts())
+adata
+
+# Quality Control
+
+# mitochondrial genes, "MT-" for human, "Mt-" for mouse
+adata.var["mt"] = adata.var_names.str.startswith("MT-")
+# ribosomal genes
+adata.var["ribo"] = adata.var_names.str.startswith(("RPS", "RPL"))
+# hemoglobin genes
+adata.var["hb"] = adata.var_names.str.startswith("^HB[^(P)]")
+
+sc.pp.calculate_qc_metrics(
+    adata, qc_vars=['mt','ribo','hb'], inplace=True, log1p=True
+)
+
+sc.pl.violin(
+    adata,
+    ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'],
+    jitter=0.4,
+    multi_panel=True,
+)
+# We can now visualize QC metrics
+# - the number of genes expressed in the count matrix
+# - the total counts per cell
+# - the percentage of counts in mitochondrial genes
+
+# We can also consider QC metrics jointly by inspecting a scatter plot colored by pct_counts_mt
+sc.pl.scatter(adata, "total_counts", "n_genes_by_counts", color="pct_counts_mt")
+
+
+
